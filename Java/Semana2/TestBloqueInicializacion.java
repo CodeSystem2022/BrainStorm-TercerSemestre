102178f480a0239f24ad322fa37b40b8b6c91ab6
Dani Dasilva <113066584+Dvni22@users.noreply.github.com>
2023-05-14 12:09:33 -0300

    Create TestBloqueInicializacion.java

---
+++ Java/Semana2/TestBloqueInicializacion.java
@@ -0,0 +1,10 @@
+package test;
+
+import domain.Persona;
+
+public class TestBloqueInicialización {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        System.out.println("persona1= " + persona1);
+    }
+}
