13cace21a132d6812ea973e2d543ff33674394df
GiuntaP <113065894+GiuntaP@users.noreply.github.com>
2023-05-16 21:40:13 -0300

    Create Ciclos.py

---
+++ JavaScrip/Clase 1/Ciclos.py
@@ -0,0 +1,40 @@
+// while
+let contador = 0;
+while(contador < 3){
+    console.log(contador);
+    contador++; 
+}
+console.log("Fin del ciclo while");
+
+// do while
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++; 
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
+
+//for
+for( let contando = 0; contando < 3; contando++){
+    console.log(contando); 0, 1, 2
+}
+console.log("Fin del ciclo for");
+
+//Palabra reservada beark
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 == 0){
+        console.log(contando); //Muestra todos los pares
+        break;
+    }
+}
+console.log("Termina el ciclo al encontrar el primer número pares");
+
+//La palabra continue y Etiquetas Labels
+inicio:
+for (let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio;// Ir a la siguiente iteración
+    }
+    console.log(contando); 0
+}
+console.log("Termina el ciclo ");
