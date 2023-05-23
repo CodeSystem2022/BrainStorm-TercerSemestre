31dbef1cb77c327eb1b5461b652accad725fd27d
Jimena Mansilla <113069344+Jimena38@users.noreply.github.com>
2023-05-23 13:49:59 -0300

    Create Rectangulo.java

---
+++ Java/Senama4/ClasesAbstractas/domain/Rectangulo.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Rectangulo extends FiguraGeometrica {
+    //constructor
+    public Rectangulo(String tipoFigura) {
+        super(tipoFigura);
+    }
+    @Override
+     public void dibujar() { //Implementando el método
+        System.out.println("Se imprime un: = " +this.getClass().getSimpleName());
+    }  
+}
