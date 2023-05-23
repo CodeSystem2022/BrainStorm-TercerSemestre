f3a856dc6de48200e295dafb266db024ec7c55c3
Jimena Mansilla <113069344+Jimena38@users.noreply.github.com>
2023-05-23 13:56:26 -0300

    Create Escritor.java

---
+++ Java/Semana5/ClaseObject/domain/Escritor.java
@@ -0,0 +1,26 @@
+
+package domain;
+
+public class Escritor extends Empleados {
+    final TipoEscritura tipoEscritura;
+    
+    public Escritor(String nombre, double sueldo, TipoEscritura tipoEscritura){
+        super(nombre, sueldo);
+        this.tipoEscritura = tipoEscritura;
+    }
+    
+    //Metodo para Sobreescribir
+    @Override
+    public String obtenerDetalles(){
+        return super.obtenerDetalles()+", Tipo Escritura: "+tipoEscritura.getDescripcion();
+}
+
+    @Override
+    public String toString() {
+        return "Escritor{" + "TipoEscritura=" + tipoEscritura + '}'+" "+super.toString();
+    } 
+    
+    public TipoEscritura getTipoEscritura(){
+        return this.tipoEscritura;
+    }
+}
