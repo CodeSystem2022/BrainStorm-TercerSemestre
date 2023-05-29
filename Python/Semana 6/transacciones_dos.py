12c2c410dd47aad95898734e530bebeb3b068dfb
Jimena Mansilla <113069344+Jimena38@users.noreply.github.com>
2023-05-29 14:25:36 -0300

    Update transacciones_dos.py

--- Python/Semana 6/transacciones_dos.py
+++ Python/Semana 6/transacciones_dos.py
@@ -2,17 +2,17 @@ import psycopg2 as bd
 
 conexion = bd.connect(user='postgres', password='matiitas', host='127.0.0.1', port='5432', database='test_bd')
 try:
-      conexion.autocommit=False#Esto no deberia estar es una mala practica
+      conexion.autocommit=False #Esto no deberia estar es una mala practica
       cursor= conexion.cursor()
       sentencia='INSERT INTO persona(nombre, apellido, email)values(%s, %s,%s)'
-      valores=('Jorge','Prol','jprol@gmail.com')#rollback maximo 10 caracteres
+      valores=('Jorge','Prol','jprol@gmail.com') #rollback maximo 10 caracteres
       cursor.execute(sentencia, valores)
 
       sentencia='UPDATE persona SET nombre=%s,apellido=%s,email=%s WHERE id_persona=%s'
       valores=('Juan Carlos','Perez','jcperez@gmail.com',14)
       cursor.execute(sentencia,valores)
 
-      conexion.commit()#Hacemos el commit manualmente esto si es una buena practica
+      conexion.commit() #Hacemos el commit manualmente esto si es una buena practica
       print('Termina la transaccion')
 
 except Exception as e:
