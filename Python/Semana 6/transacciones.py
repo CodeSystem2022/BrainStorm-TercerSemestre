201937a84887babfe9760a411008b7391877f362
Jimena Mansilla <113069344+Jimena38@users.noreply.github.com>
2023-05-29 14:23:34 -0300

    Create transacciones.py

---
+++ Python/Semana 6/transacciones.py
@@ -0,0 +1,18 @@
+import psycopg2 as bd
+
+conexion = bd.connect(user='postgres', password='matiitas', host='127.0.0.1', port='5432', database='test_bd')
+try:
+     conexion.autocommit=False#Esto no deberia estar es una mala practica
+     cursor= conexion.cursor()
+     sentencia='INSERT INTO persona(nombre, apellido, email)values(%s, %s,%s)'
+     valores=('Maria','Esparza','mesparza@gmail.com')
+     cursor.execute(sentencia, valores)
+     conexion.commit()#Hacemos el commit manualmente esto si es una buena practica
+     print('Termina la transaccion')
+
+except Exception as e:
+      conexion.rollback()
+      print(f'Ocurrio un error, se hizo un rollback:{e}')
+finally:
+      conexion.close()
+# https://www.psycopg.org/docs/usage.html
